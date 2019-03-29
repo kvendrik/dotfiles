4696ceeb2a4a2e9506cb5ee4a3473afd8ad5d75c
Koen Vendrik <k.vendrik@gmail.com>
2019-03-29 10:56:40 -0400

    fixes rps condition

--- file-system/rps.bash
+++ file-system/rps.bash
@@ -1,7 +1,7 @@
 REPOSITORY_DIRECTORY="$HOME/Desktop/repos"
 
 function rps() {
-  [ -z "$1" ] && cd "$REPOSITORY_DIRECTORY/$1" || cd "$REPOSITORY_DIRECTORY"
+  [ -z "$1" ] && cd "$REPOSITORY_DIRECTORY" || cd "$REPOSITORY_DIRECTORY/$1"
 }
 
 complete -W "$(ls "$REPOSITORY_DIRECTORY")" rps
