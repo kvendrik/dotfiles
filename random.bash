452574b0a0a706485de3f6694dc8be230ffa767e
Koen Vendrik <k.vendrik@gmail.com>
2019-05-29 16:36:29 -0400

    adds reload alias

--- random.bash
+++ random.bash
@@ -2,3 +2,7 @@
 
 alias c=clear
 alias devr="dev down; dev up; dev server"
+
+# Reload the shell (i.e. invoke as a login shell)
+# Source: https://github.com/mathiasbynens/dotfiles/blob/5368015b53467949c36f1e386582ac066b0d0ae6/.aliases#L148
+alias reload="exec ${SHELL} -l"
