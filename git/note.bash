7d7cf2cea0425f0adfe8e0f8150075ac8915d219
Koen Vendrik <k.vendrik@gmail.com>
2019-06-03 19:01:57 -0400

    cleans up note

--- git/note.bash
+++ git/note.bash
@@ -2,12 +2,12 @@
 
 __note_folder="$DOTFILES_DIRECTORY/.notes"
 
-function __note_help() {
-cat << EndOfMessage
-Note
+function note() {
+  local help_message cmd item_name storage_folder arguments
+  read -d '' help_message << EOF
 Simple note taking system for quick note taking while working that saves notes to a Github Gist.
 
-Usage: note [<note_name>|rm|list|help|update|path|open]
+Usage: note [<note_name>|rm|list|help|update|path|open] [--gist-folder=<folder_path>]
 
 Commands and arguments:
   none           Prints help message. If no Gist has been configured it will walk you 
@@ -23,40 +23,42 @@ Commands and arguments:
   update         Will pull from the remote Gist.
   path           Will print the path to the Gist folder. Remove folder
                  to delete all saved notes, this won't delet remote.
+  --gist-folder  Can be used to set a custom storage folder. Especially
+                 handy if you'd like to set up different commands
+                 that utilize this CLI to save different categories of notes.
+                 e.g. alias til="note --gist-folder='~/.til'"
 
 Troubleshooting:
 Running into trouble? You can reset your setup by removing your local copy
 of you notes (run 'note path' to view the path).
-EndOfMessage
-}
+EOF
 
-function __note_list() {
-  find "$__note_folder" -name "*" -execdir sh -c 'printf "%s\n" "${0%.*}"' {} ';' -maxdepth 1 | while read -r file_name; do
-    if [ -z "$file_name" ]; then
+  arguments=()
+  cmd="${arguments[1]}"
+  item_name="${arguments[2]}"
+  storage_folder="$__note_folder"
+
+  for argument in "$@"; do
+    if [[ -n "$(echo "$argument" | grep -Eo '\-\-gist-folder')" ]]; then
+      storage_folder="$(echo "$argument" | grep -Eo '\=.+' | tr -d '=')"
+      if [ -z "$storage_folder" ]; then
+        echo "--gist-folder argument '$storage_folder' is invalid."
+        return 1
+      fi
       continue
     fi
-    $1 "$file_name"
+
+    arguments+=("$argument")
   done
-}
 
-function __note_add_compreply() {
-  COMPREPLY+=("$1")
-}
-
-function __note_autocomplete() {
-  __note_list __note_add_compreply
-}
-
-function note() {
-  if [[ "$1" == 'help' ]]; then
-    __note_help
+  if [[ "$cmd" == 'help' ]]; then
+    echo "$help_message"
     return
   fi
 
-  if [ ! -d "$__note_folder" ]; then
+  if [ ! -d "$storage_folder" ]; then
     cat << EndOfMessage
-
-No Github Gist ID found.
+No Github Gist ID found. (Using folder '$storage_folder')
 
 1. Head over to https://gist.github.com/ to create a Gist for your notes.
 2. Paste the Gist ID (last part of the Gist URL) below and press enter.
@@ -67,101 +69,106 @@ EndOfMessage
     echo -n 'Gist ID: '
     local gist_id
     read -r gist_id
-    mkdir -p "$__note_folder"
-    git init "$__note_folder"
-    git -C "$__note_folder" remote add origin "git@gist.github.com:$gist_id.git"
-    git -C "$__note_folder" pull origin master
+    mkdir -p "$storage_folder"
+    git init "$storage_folder"
+    git -C "$storage_folder" remote add origin "git@gist.github.com:$gist_id.git"
+    git -C "$storage_folder" pull origin master
     return
   fi
 
-  if [[ "$1" == 'path' ]]; then
-    echo "$__note_folder"
+  if [[ "$cmd" == 'path' ]]; then
+    echo "$storage_folder"
     return
   fi
 
-  if [[ "$1" == 'update' ]]; then
-    git -C "$__note_folder" pull origin master
+  if [[ "$cmd" == 'update' ]]; then
+    git -C "$storage_folder" pull origin master
     return
   fi
 
-  if [[ "$1" == 'open' ]]; then
+  if [[ "$cmd" == 'open' ]]; then
     local gist_id
-    gist_id="$(git -C "$__note_folder" remote -v | grep -Eo -m1 '\:([^\.]+)' | tr -d ':')"
+    gist_id="$(git -C "$storage_folder" remote -v | grep -Eo -m1 '\:([^\.]+)' | tr -d ':')"
     open "https://gist.github.com/$GITHUB_USERNAME/$gist_id"
     return
   fi
 
-  if [[ "$1" == 'edit' ]]; then
-    if [ -z "$2" ]; then
+  if [[ "$cmd" == 'edit' ]]; then
+    if [ -z "$item_name" ]; then
       echo "Usage: note edit <note_name>. Run 'note list' to see all notes."
       return 1
     fi
-    if [ ! -f "$__note_folder/$2.md" ]; then
-      echo "No note called '$2' found. Run 'note list' to see all notes."
+    if [ ! -f "$storage_folder/$item_name.md" ]; then
+      echo "No note called '$item_name' found. Run 'note list' to see all notes."
       return
     fi
-    vim "$__note_folder/$2.md"
-    git -C "$__note_folder" add --all :/
-    git -C "$__note_folder" commit -m "Edited $2"
-    git -C "$__note_folder" push origin master
+    vim "$storage_folder/$2.md"
+    git -C "$storage_folder" add --all :/
+    git -C "$storage_folder" commit -m "Edited $2"
+    git -C "$storage_folder" push origin master
     return
   fi
 
-  if [[ "$1" == 'rm' ]]; then
-    if [ -z "$2" ]; then
+  if [[ "$cmd" == 'rm' ]]; then
+    if [ -z "$item_name" ]; then
       echo "Usage: note rm <note_name>. Run 'note list' to see all notes."
       return 1
     fi
-    if [ ! -f "$__note_folder/$2.md" ]; then
-      echo "No note called '$2' found. Run 'note list' to see all notes."
+    if [ ! -f "$storage_folder/$item_name.md" ]; then
+      echo "No note called '$item_name' found. Run 'note list' to see all notes."
       return
     fi
-    rm "$__note_folder/$2.md"
-    git -C "$__note_folder" add --all :/
-    git -C "$__note_folder" commit -m "Removed $2"
-    git -C "$__note_folder" push origin master
+    rm "$storage_folder/$2.md"
+    git -C "$storage_folder" add --all :/
+    git -C "$storage_folder" commit -m "Removed $2"
+    git -C "$storage_folder" push origin master
     return
   fi
 
   if [[ "$1" == 'list' ]]; then
-    __note_list 'echo'
+    __list_directory_as_items "$storage_folder" 'echo'
     return
   fi
 
-  local file_name
-  file_name="$1"
+  local file_name item_value
+  file_name="$cmd"
+  item_value="$item_name"
 
-  if [ -f "$__note_folder/$file_name.md" ]; then
-    if [ -z "$2" ]; then
-      cat "$__note_folder/$file_name.md"
+  if [ -f "$storage_folder/$file_name.md" ]; then
+    if [ -z "$item_value" ]; then
+      cat "$storage_folder/$file_name.md"
       return
     fi
-    echo "$2" >> "$__note_folder/$file_name.md"
-    git -C "$__note_folder" add "$full_file_name"
-    git -C "$__note_folder" commit -m "Added $full_file_name"
-    git -C "$__note_folder" push origin master
+    echo "$item_value" >> "$storage_folder/$file_name.md"
+    git -C "$storage_folder" add "$full_file_name"
+    git -C "$storage_folder" commit -m "Added $full_file_name"
+    git -C "$storage_folder" push origin master
     return
   fi
 
   local full_file_name
-  file_name="$(echo "$1" | tr ' ' '-')"
+  file_name="$(echo "$cmd" | tr ' ' '-')"
 
   if [ -z "$file_name" ]; then
-    __note_help
+    echo "$help_message"
     return
   fi
 
   full_file_name="$file_name.md"
 
-  if [ -z "$2" ]; then
-    vim "$__note_folder/$full_file_name"
+  if [ -z "$item_value" ]; then
+    vim "$storage_folder/$full_file_name"
   else
-    echo "$2" > "$__note_folder/$full_file_name"
+    echo "$item_value" > "$storage_folder/$full_file_name"
   fi
 
-  git -C "$__note_folder" add "$full_file_name"
-  git -C "$__note_folder" commit -m "Added $full_file_name"
-  git -C "$__note_folder" push origin master
+  git -C "$storage_folder" add "$full_file_name"
+  git -C "$storage_folder" commit -m "Added $full_file_name"
+  git -C "$storage_folder" push origin master
+}
+
+function __note_autocomplete() {
+  __list_directory_as_items "$__note_folder" __add_to_compreply
 }
 
 complete -F __note_autocomplete note
