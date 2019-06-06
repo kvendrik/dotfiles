0db1d01a73ae80ff94119f56a62a28688c25728b
Koen Vendrik <k.vendrik@gmail.com>
2019-06-06 19:51:57 -0400

    fixes linting

--- random.bash
+++ random.bash
@@ -7,4 +7,4 @@ alias devr="dev down; dev up; dev server"
 # Source: https://github.com/mathiasbynens/dotfiles/blob/5368015b53467949c36f1e386582ac066b0d0ae6/.aliases#L148
 alias reload='exec ${SHELL} -l'
 
-alias config-backups="$DOTFILES_DIRECTORY/bootstrap/config-backups"
+alias config-backups='$DOTFILES_DIRECTORY/bootstrap/config-backups'
