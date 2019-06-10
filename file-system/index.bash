a9ec142b847e28c438d5c725fca32e07fb4e6dd1
Koen Vendrik <k.vendrik@gmail.com>
2019-06-10 11:23:39 -0400

    improves co command

--- file-system/index.bash
+++ file-system/index.bash
@@ -3,7 +3,7 @@
 
 FILE_SYSTEM_DIR=$(dirname "$0")
 
-source "$FILE_SYSTEM_DIR/miscellaneous.bash"
 source "$FILE_SYSTEM_DIR/rps.bash"
+source "$FILE_SYSTEM_DIR/miscellaneous.bash"
 
 unset FILE_SYSTEM_DIR
--- file-system/miscellaneous.bash
+++ file-system/miscellaneous.bash
@@ -7,8 +7,32 @@ alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 alias cdf='cd $(osascript -e "tell app \"Finder\" to POSIX path of (insertion location as alias)")'
 
 function co() {
-  code "${@:-.}"
+  local open_path other_args
+  open_path="$1"
+  other_args=("${@:2}")
+
+  if [ -z "$open_path" ]; then
+    code . "${other_args[@]}"
+    return
+  fi
+
+  if [ -d "$open_path" ] || [ -f "$open_path" ]; then
+    code "$open_path" "${other_args[@]}"
+    return
+  fi
+
+  local repository_path
+  repository_path="$REPOSITORIES_DIRECTORY/$1"
+
+  if [ -d "$repository_path" ]; then
+    code "$repository_path" "${other_args[@]}"
+    return
+  fi
+
+  code "$open_path" "${other_args[@]}"
 }
+
+rps_autocomplete co
 
 function o() {
   open -a Finder "$1"
