75520f44930e759c6601423d82e064d1be2839de
Koen Vendrik <k.vendrik@gmail.com>
2019-11-11 17:56:19 -0500

    moves DOTFILES_DIRECTORY to foundation

--- foundation/index.bash
+++ foundation/index.bash
@@ -2,6 +2,7 @@
 # shellcheck source=./foundation/index.bash
 
 FOUNDATION_DIR=$(dirname "$0")
+DOTFILES_DIRECTORY=$(dirname "$FOUNDATION_DIR")
 
 source "$FOUNDATION_DIR/config.bash"
 source "$FOUNDATION_DIR/utilities.bash"
