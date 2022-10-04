39a8ed5b3811a7243f75d6febb3baca6eaf7f0f9
Koen Vendrik <k.vendrik@gmail.com>
2022-10-04 13:16:07 +0200

    fixes spin commands

--- spin.bash
+++ spin.bash
@@ -6,24 +6,31 @@ s() {
   local cmd instance_id do_destroy
   cmd="$1"
 
-  [ -z "$cmd" ] && echo 'Usage: s <up|down>' && return 1
+  [ -z "$cmd" ] && echo 'Usage: s <up|clean|ssh>' && return 1
 
-  if [[ "$cmd" == "up" ]]; then
+  if [[ "$cmd" == "up" ]] || [[ "$cmd" == "u" ]]; then
     ([ -z "$2" ] || [ -z "$3" ]) && echo 'Usage: s up <repo> <name>' && return 1
     spin up "$2" --name="$3" && spin open && spin code && spin ssh
     return 0
   fi
 
-  if [[ "$cmd" == "down" ]]; then
+  if [[ "$cmd" == "clean" ]] || [[ "$cmd" == "c" ]]; then
+    spin list
+
+    printf "%s" "Destroy all instances? [y/N] "
+    read -r do_destroy
+    [[ "$do_destroy" != 'y' ]] && return 0
+
     for instance_id in `spin list | grep -Eo "^[^ ]+" | tail -n +2`; do
-      printf "%s" "Destroy $instance_id? [y/N] "
-      read -r do_destroy
-      [[ "$do_destroy" != 'y' ]] && continue
+      echo ">>> Destroying $instance_id"
       spin destroy "$instance_id"
     done
+
+    echo "Done."
+
     spin list
     return 0
   fi
 
-  [[ "$cmd" == "ssh" ]] && spin ssh
+  [[ "$cmd" == "ssh" ]] || [[ "$cmd" == "s" ]] && spin ssh
 }
