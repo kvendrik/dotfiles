d9229096317aa5d145557d6943bc15fbc4966919
Koen Vendrik <k.vendrik@gmail.com>
2023-03-09 17:27:53 +0100

    fixes folder

--- file-system/miscellaneous.bash
+++ file-system/miscellaneous.bash
@@ -63,6 +63,6 @@ rn() {
 
 scaffold() {
   export SCAFFOLD_RUN="1"  
-  scaffold_project $@ && [ -n "$1" ] && [ -n "$2" ] && [ "$1" != "nvm" ] && cd "$HOME/Desktop/$(ls -t "$HOME/Desktop" | head -1)"
+  scaffold_project $@ && [ -n "$1" ] && [ -n "$2" ] && [ "$1" != "nvm" ] && cd "$SCAFFOLD_FOLDER/$(ls -t "$SCAFFOLD_FOLDER" | head -1)"
   unset SCAFFOLD_RUN
 }
