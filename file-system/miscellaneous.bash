eb260e8efd9d799845304aec835041debf78cea7
Koen Vendrik <k.vendrik@gmail.com>
2023-03-10 14:11:26 +0100

    adds create script

--- file-system/miscellaneous.bash
+++ file-system/miscellaneous.bash
@@ -64,7 +64,7 @@ rn() {
 
 scaffold() {
   export SCAFFOLD_RUN="1"  
-  scaffold_project $@ && [ -n "$1" ] && [ -n "$2" ] && [ "$1" != "nvm" ] && cd "$SCAFFOLD_FOLDER/$(ls -t "$SCAFFOLD_FOLDER" | head -1)"
+  scaffold_project $@ && [ -n "$1" ] && [ -n "$2" ] && [ "$1" != "nvm" ] && [ "$1" != "rm" ] && cd "$SCAFFOLD_FOLDER/$(ls -t "$SCAFFOLD_FOLDER" | head -1)" 
   [ "$1" = "nvm" ] && cd "$SCAFFOLD_FOLDER"
   unset SCAFFOLD_RUN
 }
