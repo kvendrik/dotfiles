46d730bf8e37df238ded324f20867331fce5616d
Koen Vendrik <k.vendrik@gmail.com>
2024-03-19 15:58:49 +0000

    --push adds squash utilities

--- git/git.bash
+++ git/git.bash
@@ -8,10 +8,6 @@ _git_check_uncommited_changes() {
   git diff-index --quiet HEAD -- || echo "uncommited changes found"
 }
 
-_git_check_unpushed_changes() {
-  git log origin/$(_git_current_branch)..HEAD
-}
-
 _folder_name_from_git_uri() {
   basename "$1" .git
 }
@@ -57,117 +53,25 @@ alias gr="git rebase -i"
 alias gcom='git checkout $(_git_main_branch)'
 alias amend='git commit --amend'
 
-cont() {
-  [ -n "$(_git_check_uncommited_changes)" ] && _git_commit
+continue() {
+  [ -n "$(_git_check_uncommited_changes)" ] && echo "Commit changes first." && return 1
   git rebase --continue
 }
 
-fresh() {
-  local branch_name current_branch
-  current_branch="$(_git_current_branch)"
-  branch_name="$([ -z "$1" ] && echo "$current_branch" || echo "$1")"
-
-  if [ -n "$(_git_check_uncommited_changes)" ]; then
-    echo 'Uncommited changes found. Please commit/stash those first.'
-    return 1
-  fi
-
-  echo "Creating fresh copy of $branch_name"
-
-  if [[ "$branch_name" == "$current_branch" ]]; then
-    git checkout "$(_git_main_branch)"
-  fi
-
-  git rev-parse --verify "$branch_name" &> /dev/null && git branch -D "$branch_name" &> /dev/null
-  git fetch origin "$branch_name" && git checkout "$branch_name"
-}
-
 squash() {
-  local main_branch commit_message
-
-  main_branch="$(_git_main_branch)"
-  commit_message="$@"
-
-  if [ -n "$(_git_check_uncommited_changes)" ]; then
-    echo "Commit changes first."
-    return 1
-  fi
-
-  if [ -n "$(_git_check_unpushed_changes)" ]; then
-    echo "Push changes first so you have a backup in case something goes awry."
-    return 1
-  fi
-
-  if [ -z "$commit_message" ]; then
-    echo "No commit message given. Usage: squash <commit_message>"
-    return
-  fi
-
-  git fetch origin "$main_branch" && git-squash --base="origin/$main_branch" "$commit_message" && echo "✅ Done. Please check commits and force push."
-}
-
-ub() {
-  if [ -n "$(_check_contains_flag "$*" 'help' 'h')" ]; then
-    echo "Usage: ub [--merge|-m] [<base_branch>]. Updates base_branch and rebases it on top of your current branch."
-    return
-  fi
-
-  # shellcheck disable=SC2086,SC2048
-  _strip_flags $*
-
-  local base_branch
-  base_branch="${CURRENT_CLEAN_ARGUMENTS[1]:-$(_git_main_branch)}"
-
-  if [ -n "$(_check_contains_flag "$*" 'merge' 'm')" ]; then
-    git fetch origin "$base_branch" && git merge "$base_branch"
-    return
-  fi
-
-  git fetch origin "$base_branch" && git rebase -i "origin/$base_branch"
-}
-
-branch-diff() {
-  local commit_base branch_name base_branch
-
-  if [ -n "$(_check_contains_flag "$*" 'help' 'h')" ]; then
-    echo "Usage: branch-diff [<base_branch>] [<branch_name>] [--files|-f]
-
-Get the diff between a branch and some base branch.
-
-Arguments
-base_branch            name of the branch to compare to. $(_git_main_branch) by default.
-branch_name            name of the branch to compare. HEAD by default.
-
-Flags
---files|-f             only show file paths"
-    return
-  fi
-
-  # shellcheck disable=SC2086,SC2048
-  _strip_flags $*
-  base_branch="${CURRENT_CLEAN_ARGUMENTS[1]:-$(_git_main_branch)}"
-  branch_name="${CURRENT_CLEAN_ARGUMENTS[2]:-HEAD}"
-  commit_base="$(git merge-base "$base_branch" "$branch_name")"
-
-  if [ -n "$(_check_contains_flag "$*" 'files' 'f')" ]; then
-    git diff --name-only "$commit_base" "$branch_name"
-    return
-  fi
-
-  git diff "$commit_base" "$branch_name"
+  [ -n "$(_git_check_uncommited_changes)" ] && echo "Commit changes first." && return 1
+  local branch_name commit_message
+  branch_name="$1"
+  commit_message="${@:2}"
+  GIT_EDITOR="sed -i -e '2 s/^#/$commit_message\'$'\n&/g'" GIT_SEQUENCE_EDITOR="sed -i -e '1 ! s/pick/squash/g'" git rebase -i "$branch_name"
 }
 
 # Git Checkout Recent
 # Shows list of recently used branches
-
 which gcor &> /dev/null && unalias gcor
 
 gcor() {
   git reflog | grep -Eo 'moving from [^ ]+' | grep -Eo '[^ ]+$' | awk '!a[$0]++' | head -n 20 | awk '{if(system("[ -z \"$(git branch --list "$0")\" ]")){print}}' | fzf | xargs git checkout
-}
-
-gcol() {
-  git reflog | grep -Eo 'moving from [^ ]+' | grep -Eo '[^ ]+$' | awk '!a[$0]++' | head -n 1 | awk '{if(system("[ -z \"$(git branch --list "$0")\" ]")){print}}' | xargs git checkout
 }
 
 gccd() {
