bd9a0fb8e190d532f2d1b2e30afe0e12d71707fd
Koen Vendrik <k.vendrik@gmail.com>
2024-12-17 16:10:51 +0100

    adds gr alias

--- git/git.bash
+++ git/git.bash
@@ -53,36 +53,8 @@ alias gr="git rebase -i"
 alias gcom='git checkout $(_git_main_branch)'
 alias amend='git commit --amend'
 
-# squash() {
-#   local branch_name upstream_name commit_message
-
-#   if [[ "$1" == "continue" ]]; then
-#     [ -n "$(_git_check_uncommited_changes)" ] && git commit
-#     git rebase --continue
-#     return
-#   fi
-
-#   branch_name="$1"
-#   commit_message="${@:2}"
-
-#   [ -z "$branch_name" ] && echo "Please provide a branch name to rebase onto." && return 1
-#   [ -n "$(echo "$branch_name" | grep "/")" ] && echo "Please provide a name without the upstream. So ’main’ instead of ’origin/main’" && return 1
-
-#   if [ -n "$(_git_check_uncommited_changes)" ]; then
-#     [ -z "$commit_message" ] && echo "Please provide a commit message, or commit your changes before running the command again." && return 1
-#     git commit -m "$commit_message"
-#   fi
-
-#   [ -z "$commit_message" ] && echo "Please provide a commit message for the rebase." && return 1
-
-#   upstream_name="origin"
-
-#   echo "Branch: $upstream_name/$branch_name"
-#   echo "Commit message: $commit_message"
-#   echo "---"
-
-#   git fetch "$upstream_name" "$branch_name" && GIT_EDITOR="sed -i -e '2 s/^#/$commit_message\''\n&/g'" GIT_SEQUENCE_EDITOR="sed -i -e '1 ! s/pick/squash/g'" git rebase -i "$upstream_name/$branch_name"
-# }
+which gr &> /dev/null && unalias gr
+alias gr="git fetch origin main && git rebase -i origin/main"
 
 # Git Checkout Recent
 # Shows list of recently used branches
